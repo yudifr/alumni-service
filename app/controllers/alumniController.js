ec5cae13df1b78612e37c6d1bbac962cb694f615
yudi <yudi@tsp.dev>
2023-04-15 23:23:21 +0700

    alumni

--- app/controllers/alumniController.js
+++ app/controllers/alumniController.js
@@ -1,130 +1,116 @@
-const db = require('../database/pool');
-const response = require('../helper/responses');
-exports.getAlumni = (async (req, res) => {
-    var query = "select * from alumni ";
-    try {
-        const result = await db.query(query);
+const db = require("../database/pool");
+const response = require("../helper/responses");
+exports.getAlumni = async (req, res) => {
+  var query = "select * from alumni ";
+  try {
+    const result = await db.query(query);
 
-        return response.ok('ok', result.rows, res);
-
-    } catch (error) {
-        return response.badRequest(error, error, res);
+    return response.ok("ok", result.rows, res);
+  } catch (error) {
+    return response.badRequest(error, error, res);
+  }
+};
+exports.getAlumniId = async (req, res) => {
+  var query = "select * from alumni where alumni.id = " + req.params.id;
+  try {
+    const result = await db.query(query);
+    if (result.rowCount == 0) {
+      return response.badRequest("no data", res);
     }
-});
-exports.getAlumniId = (async (req, res) => {
-    var query = "select * from alumni where alumni.id = " + req.params.id;
-    try {
-        const result = await db.query(query);
-        if (result.rowCount == 0) {
-            return response.badRequest('no data', res)
-        }
-        return response.ok('ok', result.rows, res);
-
-    } catch (error) {
-        return response.badRequest(error, res);
+    return response.ok("ok", result.rows, res);
+  } catch (error) {
+    return response.badRequest(error, res);
+  }
+};
+exports.getAlumniRiwayatId = async (req, res) => {
+  var query =
+    "select * from alumni inner join riwayat_kerja on alumni.id = riwayat_kerja.id_alumni where alumni.id = " +
+    req.params.id;
+  try {
+    const result = await db.query(query);
+    if (result.rowCount == 0) {
+      return response.badRequest("no data", res);
     }
-});
-exports.getAlumniRiwayatId = (async (req, res) => {
-    var query = "select * from alumni inner join riwayat_kerja on alumni.id = riwayat_kerja.id_alumni where alumni.id = " + req.params.id;
-    try {
-        const result = await db.query(query);
-        if (result.rowCount == 0) {
-            return response.badRequest('no data', res)
-        }
-        return response.ok('ok', result.rows, res);
+    return response.ok("ok", result.rows, res);
+  } catch (error) {
+    return response.badRequest(error, res);
+  }
+};
 
-    } catch (error) {
-        return response.badRequest(error, res);
-    }
-});
-
-exports.newAlumni = (async (req, res) => {
-    const {
-        idPelajar,
-        lulusanSd,
-        tahunSd,
-        lulusanSmp,
-        tahunSmp,
-        lulusanSma,
-        tahunSma,
-        lulusanPt,
-        tahunPt,
-    } = req.body;
-    var query = `
-        INSERT INTO ALUMNI (id_pelajar,lulusan_sd,tahun_sd,lulusan_smp,tahun_smp,lulusan_sma,tahun_sma,lulusan_pt,tahun_pt) 
-        VALUES('${idPelajar}','${lulusanSd}','${tahunSd}','${lulusanSmp}','${tahunSmp}'
-        ,'${lulusanSma}','${tahunSma}','${lulusanPt}','${tahunPt}')
+exports.newAlumni = async (req, res) => {
+  const {
+    idPelajar,
+    id_sd,
+    tahunSd,
+    id_smp,
+    tahunSmp,
+    id_sma,
+    tahunSma,
+    id_pt,
+    tahunPt,
+  } = req.body;
+  var query = `
+        INSERT INTO ALUMNI (id_pelajar,id_sd,tahun_sd,id_smp,tahun_smp,id_sma,tahun_sma,id_pt,tahun_pt) 
+        VALUES('${idPelajar}','${id_sd}','${tahunSd}','${id_smp}','${tahunSmp}'
+        ,'${id_sma}','${tahunSma}','${id_pt}','${tahunPt}') RETURNING id
         `;
-    try {
-        const result = await db.query(query);
-        return response.ok('ok', 'data created', res);
-    } catch (error) {
-        return response.badRequest(error, res);
-    }
-});
+  console.log(query);
+  try {
+    const result = await db.query(query);
+    return response.ok("ok", result.rows, res);
+  } catch (error) {
+    console.log(error);
+    return response.badRequest(error, res);
+  }
+};
 
-exports.newRiwayat = (async (req, res) => {
-    const {
-        idAlumni,
-        idPerusahaan,
-        status,
-        tahun
-    } = req.body;
-    // insert into riwayat_kerja (id_alumni,id_perusahaan,status,tahun) values ('3','2','bekerja','2021')
-    var query = `
+exports.newRiwayat = async (req, res) => {
+  const { idAlumni, idPerusahaan, status, tahun } = req.body;
+  // insert into riwayat_kerja (id_alumni,id_perusahaan,status,tahun) values ('3','2','bekerja','2021')
+  var query = `
         INSERT INTO RIWAYAT_KERJA (id_alumni,id_perusahaan,status,tahun)
-        VALUES('${idAlumni}','${idPerusahaan}','${status}','${tahun}')
+        VALUES('${idAlumni}','${idPerusahaan}','${status}','${tahun}') RETURNING id
         `;
-    try {
-        const result = await db.query(query);
-        return response.ok('ok', result, res);
-    } catch (error) {
-        return response.badRequest(error, res);
-    }
-});
+  console.log(query);
+  try {
+    const result = await db.query(query);
+    return response.ok("ok", result.rows, res);
+  } catch (error) {
+    console.log(error);
+    return response.badRequest(error, res);
+  }
+};
 
-exports.updateAlumni = (async (req, res) => {
-    const {
-        lulusanSd,
-        tahunSd,
-        lulusanSmp,
-        tahunSmp,
-        lulusanSma,
-        tahunSma,
-        lulusanPt,
-        tahunPt
-    } = req.body;
-    const id = req.params.id;
-    var query = `
+exports.updateAlumni = async (req, res) => {
+  const { id_sd, tahunSd, id_smp, tahunSmp, id_sma, tahunSma, id_pt, tahunPt } =
+    req.body;
+  const id = req.params.id;
+  var query = `
         UPDATE ALUMNI SET
-        lulusan_sd = '${lulusanSd}',
+        id_sd = '${id_sd}',
         tahun_sd = '${tahunSd}',
-        lulusan_smp = '${lulusanSmp}',
+        id_smp = '${id_smp}',
         tahun_smp = '${tahunSmp}',
-        lulusan_sma = '${lulusanSma}',
+        id_sma = '${id_sma}',
         tahun_sma = '${tahunSma}',
-        lulusan_pt = '${lulusanPt}',
+        id_pt = '${id_pt}',
         tahun_pt = '${tahunPt}'
         WHERE id = ${id}
         `;
-    try {
-        const result = await db.query(query);
-        if (result.rowCount == 0) {
-            return response.badRequest('no related data', res)
-        }
-        return response.ok('ok', result, res);
-    } catch (error) {
-        return response.badRequest(error, res);
+  try {
+    const result = await db.query(query);
+    if (result.rowCount == 0) {
+      return response.badRequest("no related data", res);
     }
-});
-exports.updateRiwayat = (async (req, res) => {
-    const {
-        idPerusahaan,
-        status,
-        tahun
-    } = req.body;
-    const id = req.params.id;
-    var query = `
+    return response.ok("ok", result, res);
+  } catch (error) {
+    return response.badRequest(error, res);
+  }
+};
+exports.updateRiwayat = async (req, res) => {
+  const { idPerusahaan, status, tahun } = req.body;
+  const id = req.params.id;
+  var query = `
         UPDATE RIWAYAT_KERJA SET
         id_perusahaan = '${idPerusahaan}',
         status = '${status}',
@@ -132,25 +118,21 @@ exports.updateRiwayat = (async (req, res) => {
         
         WHERE id_alumni = ${id}
         `;
-    try {
-        const result = await db.query(query);
-        if (result.rowCount == 0) {
-            return response.badRequest('no related data', res)
-        }
-        return response.ok('ok', result, res);
-    } catch (error) {
-        return response.badRequest(error, res);
+  try {
+    const result = await db.query(query);
+    if (result.rowCount == 0) {
+      return response.badRequest("no related data", res);
     }
-});
+    return response.ok("ok", result, res);
+  } catch (error) {
+    return response.badRequest(error, res);
+  }
+};
 
-exports.updateRiwayat = (async (req, res) => {
-    const {
-        idPerusahaan,
-        status,
-        tahun
-    } = req.body;
-    const id = req.params.id;
-    var query = `
+exports.updateRiwayat = async (req, res) => {
+  const { idPerusahaan, status, tahun } = req.body;
+  const id = req.params.id;
+  var query = `
         UPDATE RIWAYAT_KERJA SET
         id_perusahaan = '${idPerusahaan}',
         status = '${status}',
@@ -158,47 +140,45 @@ exports.updateRiwayat = (async (req, res) => {
         
         WHERE id_alumni = ${id}
         `;
-    try {
-        const result = await db.query(query);
-        if (result.rowCount == 0) {
-            return response.badRequest('no related data', res)
-        }
-        return response.ok('ok', result, res);
-    } catch (error) {
-        return response.badRequest(error, res);
+  try {
+    const result = await db.query(query);
+    if (result.rowCount == 0) {
+      return response.badRequest("no related data", res);
     }
-});
-exports.deleteAlumni = (async (req, res) => {
-    
-    const id = req.params.id;
-    var query = `
+    return response.ok("ok", result, res);
+  } catch (error) {
+    return response.badRequest(error, res);
+  }
+};
+exports.deleteAlumni = async (req, res) => {
+  const id = req.params.id;
+  var query = `
         delete from alumni 
         WHERE id= ${id}
         `;
-    try {
-        const result = await db.query(query);
-        if (result.rowCount == 0) {
-            return response.badRequest('no related data', res)
-        }
-        return response.ok('ok', result, res);
-    } catch (error) {
-        return response.badRequest(error, res);
+  try {
+    const result = await db.query(query);
+    if (result.rowCount == 0) {
+      return response.badRequest("no related data", res);
     }
-});
-exports.deleteRiwayat = (async (req, res) => {
-    
-    const id = req.params.id;
-    var query = `
+    return response.ok("ok", result, res);
+  } catch (error) {
+    return response.badRequest(error, res);
+  }
+};
+exports.deleteRiwayat = async (req, res) => {
+  const id = req.params.id;
+  var query = `
         delete from riwayat_kerja 
         WHERE id= ${id}
         `;
-    try {
-        const result = await db.query(query);
-        if (result.rowCount == 0) {
-            return response.badRequest('no related data', res)
-        }
-        return response.ok('ok', result, res);
-    } catch (error) {
-        return response.badRequest(error, res);
+  try {
+    const result = await db.query(query);
+    if (result.rowCount == 0) {
+      return response.badRequest("no related data", res);
     }
-});
+    return response.ok("ok", result, res);
+  } catch (error) {
+    return response.badRequest(error, res);
+  }
+};
--- app/database/migration.js
+++ app/database/migration.js
@@ -1,92 +1,99 @@
-const pool = require('./pool');
-pool.on('connect',()=>{
-    console.log('connected to db');
+const pool = require("./pool");
+pool.on("connect", () => {
+  console.log("connected to db");
 });
 
-function createAlumniTable (){
-    const query =  `CREATE TABLE IF NOT EXISTS alumni
+function createAlumniTable() {
+  const query = `CREATE TABLE IF NOT EXISTS alumni
     (id SERIAL PRIMARY KEY, 
     id_pelajar NUMERIC(15) UNIQUE NOT NULL, 
-    lulusan_sd VARCHAR(15) NOT NULL, 
+    id_sd VARCHAR(15) NOT NULL, 
     tahun_sd VARCHAR(4) NOT NULL, 
-    lulusan_smp VARCHAR(15) NOT NULL, 
+    id_smp VARCHAR(15) NOT NULL, 
     tahun_smp VARCHAR(4) NOT NULL, 
-    lulusan_sma VARCHAR(15) NOT NULL, 
+    id_sma VARCHAR(15) NOT NULL, 
     tahun_sma VARCHAR(4) NOT NULL, 
-    lulusan_pt VARCHAR(15) NOT NULL, 
+    id_pt VARCHAR(15) NOT NULL, 
     tahun_pt VARCHAR(4) NOT NULL)
     `;
 
-    pool.query(query)
-    .then((res)=>{
-        console.log(res);
-        pool.end();
-    }).catch((err)=>{
-        console.log(err);
-        pool.end();
+  pool
+    .query(query)
+    .then((res) => {
+      console.log(res);
+      pool.end();
+    })
+    .catch((err) => {
+      console.log(err);
+      pool.end();
     });
-};
-function createRiwayatTable (){
-    const query =  `CREATE TABLE IF NOT EXISTS riwayat_kerja
+}
+function createRiwayatTable() {
+  const query = `CREATE TABLE IF NOT EXISTS riwayat_kerja
     (id SERIAL PRIMARY KEY, 
     id_alumni NUMERIC(15) NOT NULL, 
     id_perusahaan NUMERIC(15) NOT NULL, 
     status VARCHAR(15) NOT NULL, 
     tahun VARCHAR(4) NOT NULL)
     `;
-    pool.query(query)
-    .then((res)=>{
-        console.log(res);
-        pool.end();
-    }).catch((err)=>{
-        console.log(err);
-        pool.end();
+  pool
+    .query(query)
+    .then((res) => {
+      console.log(res);
+      pool.end();
+    })
+    .catch((err) => {
+      console.log(err);
+      pool.end();
+    });
+}
+const dropAlumniTable = () => {
+  const query = "drop table if exists alumni";
+  pool
+    .query(query)
+    .then((res) => {
+      console.log(res);
+      pool.end();
+    })
+    .catch((err) => {
+      console.log(err);
+      pool.end();
     });
 };
-const dropAlumniTable=()=>{
-    const query = 'drop table if exists alumni';
-    pool.query(query)
-    .then((res)=>{
-        console.log(res);
-        pool.end();
-    }).catch((err)=>{
-        console.log(err);
-        pool.end();
-    });
-};
-const dropRiwayatTable=()=>{
-    const query = 'drop table if exists riwayat_kerja';
-    pool.query(query)
-    .then((res)=>{
-        console.log(res);
-        pool.end();
-    }).catch((err)=>{
-        console.log(err);
-        pool.end();
+const dropRiwayatTable = () => {
+  const query = "drop table if exists riwayat_kerja";
+  pool
+    .query(query)
+    .then((res) => {
+      console.log(res);
+      pool.end();
+    })
+    .catch((err) => {
+      console.log(err);
+      pool.end();
     });
 };
 
-const createAllTables=()=>{
-    createAlumniTable();
-    createRiwayatTable();
+const createAllTables = () => {
+  createAlumniTable();
+  createRiwayatTable();
 };
 
-const dropAllTables = ()=>{
-    dropAlumniTable();
-    dropRiwayatTable();
+const dropAllTables = () => {
+  dropAlumniTable();
+  dropRiwayatTable();
 };
 
-pool.on('remove',()=>{
-    console.log('dced');
-    process.exit(0);
-
+pool.on("remove", () => {
+  console.log("dced");
+  process.exit(0);
 });
 
-module.exports= {
-    createAllTables,
-    dropAllTables
+module.exports = {
+  createAllTables,
+  dropAllTables,
 };
 
-require('make-runnable/custom')({
-    printOutputFrame: false
-  })
+require("make-runnable/custom")({
+  printOutputFrame: false,
+});
